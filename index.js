fbfae324f2ccd2838c54febe7f3c6e9a4b6b0e44
Karolis Kimtys <karolis_k_7@hotmail.com>
2022-02-18 00:55:57 +0000

    x

--- index.js
+++ index.js
@@ -1,50 +1,66 @@
-const puppeteer = require('puppeteer')
-var nodemailer = require('nodemailer')
+puppeteer = require('puppeteer-core')
+require('dotenv').config()
+const nodemailer = require('nodemailer')
 const express = require('express')
 const app = express()
 const moment = require('moment')
-require('dotenv').config()
+var cron = require('node-cron')
 
 console.log('Started scraping on', moment().format('LLLL'))
-;(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(
-    'https://bloorhomes.com/developments/surrey/ash-green/bloor-homes-at-ash-green',
-    { waitUntil: 'networkidle2' }
-  )
+;(async function () {
+  console.log('Fetching browser')
+  const browserFetcher = puppeteer.createBrowserFetcher()
+  let revisionInfo = await browserFetcher.download('884014')
 
-  const html = await page.$eval('.development-banner__text', (e) => e.outerHTML)
+  cron.schedule('*/10 * * * * *', async () => {
+    browser = await puppeteer.launch({
+      executablePath: revisionInfo.executablePath,
+      headless: true,
+      args: ['--no-sandbox', '--disabled-setupid-sandbox']
+    })
 
-  var notes = html.split('"')[1] === 'development-banner__text'
+    console.log('Scraping...')
+    const page = await browser.newPage()
+    await page.goto(
+      'https://bloorhomes.com/developments/surrey/ash-green/bloor-homes-at-ash-green',
+      { waitUntil: 'networkidle2' }
+    )
 
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'karolis.kimtys@gmail.com',
-      pass: process.env.GOOGLE_PASSWORD
+    const html = await page.$eval(
+      '.development-banner__text',
+      (e) => e.outerHTML
+    )
+
+    var notes = html.split('"')[1] === 'development-banner__text'
+
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'karolis.kimtys@gmail.com',
+        pass: process.env.GOOGLE_PASSWORD
+      }
+    })
+
+    var mailOptions = {
+      from: 'karolis.kimtys@gmail.com',
+      to: 'karolis.kimtys@gmail.com',
+      subject:
+        notes === true
+          ? 'Bloor Homes Ash Green Not Yet Available'
+          : '***Bloor Homes Ash Green Available!!!!',
+      text: moment().format('LLLL')
     }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log('Error', error)
+      } else {
+        console.log('Email sent: ' + info.response)
+      }
+    })
+
+    await browser.close()
+
+    console.log('Scraped on', moment().format('LLLL'))
   })
-
-  var mailOptions = {
-    from: 'karolis.kimtys@gmail.com',
-    to: 'karolis.kimtys@gmail.com',
-    subject:
-      notes === true
-        ? 'Bloor Homes Ash Green Not Yet Available'
-        : '***Bloor Homes Ash Green Available!!!!',
-    text: moment().format('LLLL')
-  }
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log('Error', error)
-    } else {
-      console.log('Email sent: ' + info.response)
-    }
-  })
-
-  await browser.close()
-
-  console.log('Scraped on', moment().format('LLLL'))
 })()
---
+++ instant.js
@@ -0,0 +1,110 @@
+puppeteer = require('puppeteer-core')
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+var prettyjson = require('prettyjson')
+
+const list = {
+  TheFairways:
+    'https://bloorhomes.com/developments/berkshire/near-bracknell/the-fairways',
+  OnTheGreen:
+    'https://bloorhomes.com/developments/hampshire/basingstoke/bloor-homes-on-the-green',
+  ThameMeadows:
+    'https://bloorhomes.com/developments/oxfordshire/thame/thame-meadows',
+  LakeSideGardens:
+    'https://bloorhomes.com/developments/berkshire/arborfield/lakeside-gardens',
+  ShinfieldGardens:
+    'https://bloorhomes.com/developments/berkshire/shinfield/shinfield-gardens',
+  ShinfieldMeadows:
+    'https://bloorhomes.com/developments/berkshire/shinfield/shinfield-meadows',
+  AshGreen:
+    'https://bloorhomes.com/developments/surrey/ash-green/bloor-homes-at-ash-green',
+  RedKiteView:
+    'https://bloorhomes.com/developments/oxfordshire/watlington/red-kite-view',
+  WallingfordReach:
+    'https://bloorhomes.com/developments/oxfordshire/crowmarsh-gifford/wallingford-reach'
+}
+
+let data = []
+
+async function getData(link) {
+  const browserFetcher = puppeteer.createBrowserFetcher()
+  let revisionInfo = await browserFetcher.download('884014')
+
+  browser = await puppeteer.launch({
+    executablePath: revisionInfo.executablePath,
+    headless: true,
+    args: ['--no-sandbox', '--disabled-setupid-sandbox']
+  })
+
+  const page = await browser.newPage()
+  await page.goto(link, {
+    waitUntil: 'networkidle2'
+  })
+
+  const title = await page.evaluate(() =>
+    Array.from(
+      document.querySelectorAll('.o-development-homes__card__header'),
+      (element) => element.textContent
+    )
+  )
+
+  const body = await page.evaluate(() =>
+    Array.from(
+      document.querySelectorAll('.o-development-homes__card__body'),
+      (element) => element.textContent
+    )
+  )
+
+  const combined = await title.map(function (title, index) {
+    return {
+      title: title,
+      details: body[index]
+        .split('\n')
+        .join('-')
+        .split(' ')
+        .join('')
+        .split('-')
+        .join(' ')
+    }
+  })
+
+  const developement = {
+    Developement: link,
+    Details: combined
+  }
+
+  data.push(developement)
+
+  setTimeout(async () => {
+    await browser.close()
+  }, 60000)
+}
+
+const run = async () => {
+  await Promise.all(Object.values(list).map(async (link) => getData(link)))
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'karolis.kimtys@gmail.com',
+      pass: process.env.GOOGLE_PASSWORD
+    }
+  })
+
+  var mailOptions = {
+    from: 'Bloor Homes Scraper karolis.kimtys@gmail.com',
+    to: 'karolis.kimtys@gmail.com',
+    subject: 'Bloor Homes Developements',
+    text: JSON.stringify(data, null, '\t')
+  }
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log('Error', error)
+    } else {
+      console.log('Email sent: ' + info.response)
+    }
+  })
+}
+
+run()
