a6e4652dab64228593ac8693579cdd46963d5de2
Karolis Kimtys <karolis_k_7@hotmail.com>
2022-02-21 00:30:51 +0000

    x

--- instant.js
+++ instant.js
@@ -49,6 +49,8 @@ const getData = async (link, index, browser) => {
   setTimeout(async () => {
     console.log(`Job ${index} started`)
 
+    await page.setDefaultNavigationTimeout(0)
+
     const page = await browser.newPage()
 
     await page.goto(link, {
